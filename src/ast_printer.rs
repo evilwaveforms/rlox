53dca2ba4fff5795cb5b9f92893068e5639a3e1e
evilwaveforms <8681846+evilwaveforms@users.noreply.github.com>
2023-06-29 21:19:36 +0300

    Fix `Literal` pattern matching
    Death - Symbolic

--- src/ast_printer.rs
+++ src/ast_printer.rs
@@ -1,15 +1,22 @@
 use crate::expr::*;
+use crate::scanner::Literal;
 
-// TODO: Try visitor pattern if makes sense, fix this mess
 impl Expr {
     pub fn print(&self) -> String {
         match self {
             Expr::Binary(expr) => parenthesize(&expr.operator.lexeme, &[&expr.left, &expr.right]),
             Expr::Grouping(expr) => parenthesize(&String::from("group"), &[&expr.expression]),
-            Expr::Literal(lit) => {
-                return String::from("666");
-                // return lit.to_string()
-            }
+            Expr::Literal(lit) => match lit {
+                Literal::Str(str) => {
+                    return str.to_string();
+                }
+                Literal::Number(num) => {
+                    return num.to_string();
+                }
+                Literal::Identifier(identifier) => {
+                    return identifier.to_string();
+                }
+            },
             Expr::Unary(expr) => parenthesize(&expr.operator.lexeme, &[&expr.right]),
         }
     }
